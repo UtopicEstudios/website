dd23859259e51d5e0a1313e7ba5a88c80af67a0e
Carlos Melero <carlos.melero@utopicestudios.com>
2020-05-28 17:29:00 +0200

    First release

---
+++ aframe_anal.28e8be24.js
@@ -0,0 +1,1 @@
+!function(){var e=document.querySelector("a-scene");e&&["enter","exit"].forEach((function(n){return["ar","vr"].forEach((function(t){return e.addEventListener(n+"-"+t,(function(e){return function(e,n){ga("send",{hitType:"event",eventCategory:e,eventAction:n,eventLabel:window.location})}(n,t)}))}))}))}();
